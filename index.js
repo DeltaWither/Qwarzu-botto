ecec750f69945e140bcb569ecb6bf542f83fbaba
alf56 <alf56@aber.ac.uk>
2022-03-23 02:18:42 +0000

    changed it wtf

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
-
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { token } = require("./config.json");
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Login to Discord with your client's token
-client.login(token);
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
 
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(token);
