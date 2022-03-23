336a017de193e8e0e7ed267bddd92ee0b5105799
alf56 <alf56@aber.ac.uk>
2022-03-23 03:01:45 +0000

    Basic command handler

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-intents: ['DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILDS'] });
 const { token } = require("./config.json");
+
+const client = new Client({ 
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    intents: ['DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILDS'] 
+});
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,6 +15,33 @@ client.on("messageCreate", message => {
     if (message.content === 'ping') {
         message.reply('Pong!');
     }
+    
+    handleCommand(message);
 });
 
+function handleCommand(message) {
+    let command = message.content;
+    if(command[0] !== "?") {
+        return;
+    }
+    
+    console.log(`Command: ${command}`)
+    
+    let commandAndArgs = command.slice(1).toLowerCase().split(" ")
+    
+    console.log(`Command and arguments: ${commandAndArgs}`)
+    
+    let commandName = commandAndArgs[0];
+    let args = commandAndArgs.slice(1);
+    
+    console.log(`Command name: ${commandName}`)
+    console.log(`Arguments: ${args}`)
+    message.reply(`Command name: ${commandName} \nArguments: ${args}`)
+}
+
 client.login(token);
+
+
+
+
+
