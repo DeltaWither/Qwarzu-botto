abcda67b9b4ad829ea58ba89369c935e6f4b6f89
DeltaWither <deltawither@outlook.com>
2022-03-24 23:30:02 +0000

    Updated ?nazuna

--- commands/command_nazuna.js
+++ commands/command_nazuna.js
@@ -18,7 +18,13 @@ function nazuna(message, args) {
         "https://cdn.discordapp.com/attachments/709176649278816328/956300339588501514/8.png",
         "https://cdn.discordapp.com/attachments/709176649278816328/956300339949240330/9.png",
         "https://cdn.discordapp.com/attachments/709176649278816328/956300340179914772/10.png",
-        "https://cdn.discordapp.com/attachments/709176649278816328/956300340469309520/11.png"
+        "https://cdn.discordapp.com/attachments/709176649278816328/956300340469309520/11.png",
+        "https://cdn.discordapp.com/attachments/709176649278816328/956695742255362148/cbd14d23f4a3313d1e4e710bf2e85304.png",
+        "https://cdn.discordapp.com/attachments/709176649278816328/956695742473449582/810e7f5b7bc686bfb2fc4d4a1d91053d.png",
+        "https://cdn.discordapp.com/attachments/709176649278816328/956695742800613426/sample_93f3329ff9221c48839cb67e7b9a9277.png",
+        "https://cdn.discordapp.com/attachments/709176649278816328/956695743073226802/sample_d6aa337c8ac87abd1210e2758ff6993b.png",
+        "https://cdn.discordapp.com/attachments/709176649278816328/956695743324880926/sample_71b28df04509fe868a2cf15e74153a99.png",
+        "https://cdn.discordapp.com/attachments/709176649278816328/956695743563984896/b4e3e4bc51af2c09a4aca587c0cb7df4.png"
     ]
     
     let index = Math.floor(Math.random() * nazunaArray.length)
