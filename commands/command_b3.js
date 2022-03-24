7d7f55f5b533b748a22596646d8d224f351c3d19
DeltaWither <deltawither@outlook.com>
2022-03-24 01:32:58 +0000

    Fixed b3 command

--- commands/command_b3.js
+++ commands/command_b3.js
@@ -5,7 +5,12 @@ module.exports = {
 
 function b3(message, args) {
     const b3Array = [
-        "https/cdn.discordapp.com/avatars/273927241384329216/0aa355067048e94f23bad0998b2a9ed4.png;https://media.discordapp.net/attachments/738946290930548816/738946709731672104/4c1be7b66e33fe397875b247c5e64d21.png;https://media.discordapp.net/attachments/738946290930548816/738947258774585394/703050f69c1e7516919238d52fce5b1d.png;https://media.discordapp.net/attachments/738946290930548816/738947207402749983/a629bae01e8f41ec9be70715778dd80d.png;https://cdn.discordapp.com/attachments/738946290930548816/824059620322181130/20210322_234729.png;https://cdn.discordapp.com/attachments/738946290930548816/738946421537112204/Sin-titulo-1.png",
+        "https/cdn.discordapp.com/avatars/273927241384329216/0aa355067048e94f23bad0998b2a9ed4.png",
+        "https://media.discordapp.net/attachments/738946290930548816/738946709731672104/4c1be7b66e33fe397875b247c5e64d21.png",
+        "https://media.discordapp.net/attachments/738946290930548816/738947258774585394/703050f69c1e7516919238d52fce5b1d.png",
+        "https://media.discordapp.net/attachments/738946290930548816/738947207402749983/a629bae01e8f41ec9be70715778dd80d.png",
+        "https://cdn.discordapp.com/attachments/738946290930548816/824059620322181130/20210322_234729.png",
+        "https://cdn.discordapp.com/attachments/738946290930548816/738946421537112204/Sin-titulo-1.png",
         "https://cdn.discordapp.com/attachments/738946290930548816/738946449336959046/5f226c30ac0e4.png",
         "https://cdn.discordapp.com/attachments/738946290930548816/738946543884828677/4wz8qfkpsm853.png",
         "https://cdn.discordapp.com/attachments/738946290930548816/738946709731672104/4c1be7b66e33fe397875b247c5e64d21.png",
