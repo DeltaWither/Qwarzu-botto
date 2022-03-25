436eaeb68e3aeb3790b726df956af38e278304ca
DeltaWither <deltawither@outlook.com>
2022-03-25 15:13:30 +0000

    Added support for more roles with perm to use ?dm

--- commands/command_dm.js
+++ commands/command_dm.js
@@ -9,7 +9,7 @@ function dm(message, args) {
     var mentions = message.mentions.users.first();
     var dmMessage = message.content.slice(26).trim();
     
-    if (message.member.roles.cache.has('708716555785076798')) {
+    if (message.member.roles.cache.hasAny('708716555785076798', '708722040286478356', '945585653024964721')) {
         mentions.send(dmMessage)
     } else {
         message.channel.send("no perms")
