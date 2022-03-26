6b735daac6606fd98abbd380c1cbe1bd4d94e0aa
Max <46790388+MaxFromPoland@users.noreply.github.com>
2022-03-26 16:02:26 +0100

    dog command?
    dog command.

---
+++ commands/command_dog.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "description": "Cool dog photos",
+    "function": dog
+}
+
+function dog(message, args) {
+    const dogArray = [
+        "https://media.discordapp.net/attachments/801762830478344203/957290702063165550/samoyed_puppy_dog_pictures.png",
+        "https://media.discordapp.net/attachments/801762830478344203/957290728994770964/322868_1100-800x825.png",
+        "https://media.discordapp.net/attachments/801762830478344203/957291521999261726/3180-Pug_green_grass-732x549-thumbnail-732x549.png",
+        "https://media.discordapp.net/attachments/801762830478344203/957291566626643978/image.png"
+    ]
+        
+
+let index = Math.floor(Math.random() * dogArray.length)
+message.channel.send(dogArray[index]);
+}
