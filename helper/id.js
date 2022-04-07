7fabdcc307a5a3ce1cd8d6a49990312067b03ef5
DeltaWither <deltawither@outlook.com>
2022-04-08 00:46:05 +0200

    Added isMemberId function

--- helper/id.js
+++ helper/id.js
@@ -8,6 +8,26 @@ id.isId = (string) => {
     return Number.isInteger(Number(string));
 }
 
+id.isMemberId = async (string, guild) => {
+    if(!id.isId(string)) {
+        return false
+    }
+    
+    let member = null
+    
+    try {
+        member = await guild.members.fetch(string)
+    } catch(err) {}
+        
+    console.log(member)
+    
+    if(member) {
+        return true
+    }
+    
+    return false
+}
+
 
 
 module.exports = id
