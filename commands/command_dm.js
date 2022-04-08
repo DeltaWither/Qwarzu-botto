0521dae0c42b6f0768272881ac17a1abbf9075ab
DeltaWither <deltawither@outlook.com>
2022-04-08 03:50:40 +0200

    Updated ?dm to accept both user ids and user mentions and only in the first argument

--- commands/command_dm.js
+++ commands/command_dm.js
@@ -1,3 +1,5 @@
+const id = require("../helper/id.js")
+
 module.exports = {
     "description": "send a dm. use : ex. ?dm <@user or id> content",
     "function": dm
@@ -5,17 +7,25 @@ module.exports = {
 
 
 
-function dm(message, args) {
-    var mentions = message.mentions.users.first();
-    var dmMessage = message.content.slice(26).trim();
+async function dm(message, args) {
+    let userId
     
-    if (message.mentions.members.size > 0) {
-        if (message.member.roles.cache.hasAny('708716555785076798', '708722040286478356', '945585653024964721')) {
-            mentions.send(dmMessage)
-        } else {
-            message.channel.send("no perms")
-        }
+    if( await id.isMemberId(args[0], message.guild) ) {
+        userId = args[0]
+    } else if( await id.isMemberMention(args[0], message.guild) ) {
+        userId = args[0].slice(3, -1)
     } else {
         message.channel.send("no mention")
+        return
+    }
+    
+    let dmMessage = args.slice(1).join(" ")
+    let user = await message.guild.members.fetch(userId)
+    console.log(user)
+    
+    if (message.member.roles.cache.hasAny('708716555785076798', '708722040286478356', '945585653024964721')) {
+        user.send(dmMessage)
+    } else {
+        message.channel.send("no perms")
     }
 }
