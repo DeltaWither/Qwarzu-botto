dc5310f27bffdfba6763389fe88a65a5b07e1e91
DeltaWither <deltawither@outlook.com>
2022-04-09 23:30:52 +0200

    Added perm check for enable and disable

--- commands/command_enable.js
+++ commands/command_enable.js
@@ -3,6 +3,11 @@ const {individualListeners} = require("../listeners/listeners.js")
 
 const exec = (message, args) => {
     const listenerName = args[0]
+    
+    if (!message.member.roles.cache.hasAny('708716555785076798', '708722040286478356', '945585653024964721', '948632972427423784')) {
+        return
+    }
+    
     if(!individualListeners.hasOwnProperty(listenerName)) {
         message.channel.send(`Listener ${listenerName} doesn't exist`)
         return
