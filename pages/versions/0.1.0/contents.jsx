7de1973c19229671a597122aea54477ab3b830c2
DeltaWither <deltawither@outlook.com>
2022-06-19 04:11:00 +0200

    Made links in contents page more readable

--- pages/versions/0.1.0/contents.jsx
+++ pages/versions/0.1.0/contents.jsx
@@ -7,22 +7,26 @@ export default function v0_1_0index() {
             <p>This is the index for the documentation for version 0.1.0 of 
             Qwarzu Botto</p>
             <h3>Basic guide</h3>
-            <a href="basic/commands">Commands</a>
-            <a href="basic/listeners">Listeners</a>
-            <a href="basic/schedules">Schedules</a>
-            <a href="basic/member_groups">Member groups</a>
-            
+            <div className={styles.linklist}>
+                <a href="basic/commands">Commands</a>
+                <a href="basic/listeners">Listeners</a>
+                <a href="basic/schedules">Schedules</a>
+                <a href="basic/member_groups">Member groups</a>
+            </div>
             <h3>Medium guide</h3>
-            <a href="medium/commands">Commands</a>
-            <a href="medium/listeners">Listeners</a>
-            <a href="medium/schedules">Schedules</a>
-            <a href="medium/member_groups">Member groups</a>
-            
+            <div className={styles.linklist}>
+                <a href="medium/commands">Commands</a>
+                <a href="medium/listeners">Listeners</a>
+                <a href="medium/schedules">Schedules</a>
+                <a href="medium/member_groups">Member groups</a>
+            </div>
             <h3>Advanced guide</h3>
-            <a href="advanced/commands">Commands</a>
-            <a href="advanced/listeners">Listeners</a>
-            <a href="advanced/schedules">Schedules</a>
-            <a href="advanced/member_groups">Member groups</a>
+            <div className={styles.linklist}>
+                <a href="advanced/commands">Commands</a>
+                <a href="advanced/listeners">Listeners</a>
+                <a href="advanced/schedules">Schedules</a>
+                <a href="advanced/member_groups">Member groups</a>
+            </div>
         </div>
     )
 }
