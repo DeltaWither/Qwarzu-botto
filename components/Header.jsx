63fd77e66ff7914d163552b05d19f3dfc73aa06a
DeltaWither <deltawither@outlook.com>
2022-07-13 22:23:58 +0200

    Added header to all pages

---
+++ components/Header.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "./Header.module.css"
+
+export default function Header() {
+    return(
+        <header id={styles.mainHeader}>
+          <p>Qwarzu Botto</p>
+
+          <div className="links">
+            
+            <Link className="linkBox" href="/">
+              <a>Main page</a>
+            </Link>
+            
+            <Link className="linkBox" href="/versions/0.1.0/contents">
+              <a>0.1.x docs</a>
+            </Link>
+            
+            <a>Previous versions</a>
+            
+          </div>
+        </header>
+    );
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,11 @@
 import "./styles/globals.css"
+import Header from "../components/Header.jsx"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <>
+             <Header/>
+             <Component {...pageProps} />
+           </>
 }
 
 export default MyApp
