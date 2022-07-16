90069a19c52df60923b9890ab17e42e4ce3ea1ad
DeltaWither <deltawither@outlook.com>
2022-07-16 18:51:17 +0200

    Improved basic listener guide

--- pages/versions/0.1.x/basic/listeners.jsx
+++ pages/versions/0.1.x/basic/listeners.jsx
@@ -1,3 +1,5 @@
+import { DiscordBG, UserMsg, BotMsg } from './../../../../components/discordMsg/Discord.jsx';
+
 export default function Listeners() {
     return (
         <div className="container">
@@ -6,7 +8,8 @@ export default function Listeners() {
           <p>Listeners can be thought of as commands, but better. In fact, commands are handled by a listener,
             and I think that's really damn cool.</p>
           
-          <p>If a command executes certain code on certain keywords starting with "?", (example "?test"),
+          <p>If a command executes certain code on certain keywords starting
+            with <span className="inlineCode">?</span>, (example <span className="inlineCode">?test</span>),
             listeners execute certain code on certain Discord events, such as a message being created, a user
             joining the server, someone changing their status or nickname, etc.</p>
           
@@ -19,36 +22,53 @@ export default function Listeners() {
             is constantly happening on the server it's in, and you can use the info that comes with it to do lots
             of cool stuff.</p>
           
-          <p>The most notable example of a listener is "handleCommand". It listens for messages, checks whether
-            they are a command or not, and if they are they get executed.</p>
+          <p>The most notable example of a listener is <span className="inlineCode">handleCommand</span>. It
+            listens for messages, checks whether they are a command or not, and if they are they get
+            executed.</p>
           
           <p>A listener has a name to identify it, a description that in theory would be useful for a ?help
             command, some code to execute, and the event type. Examples of event types are message create,
             message delete and message edit.
-            <a href="https://discord.js.org/#/docs/discord.js/stable/class/Client?scrollTo=e-channelCreate">Here
+            <a href="https://discord.js.org/#/docs/discord.js/stable/class/Client?scrollTo=e-channelCreate"> Here
             </a> is the full list in the Event section</p>
           
-          <p>Listeners can be enabled or disabled. The only listener enabled by default in this version is
-            handleCommand, so if you want something like the beautiful sexCounter listener to be active, you can
-            use the commands that come with the bot:</p>
+          <p>Listeners can be enabled or disabled. The only listener enabled by default in this version
+            is <span className="inlineCode">handleCommand</span>, so if you want something like the beautiful
+            sexCounter listener to be active, you can use the commands that come with the bot:</p>
           
           <ul>
-            <li>?enable 
+            <li><span className="inlineCode">?enable</span> 
               <p>Takes one argument. It enables a listener and is available only for members with admin perm.
                 Example:</p>
               
-              <p>?enable leaguerole</p>
+              <DiscordBG>
+                <UserMsg>
+                  ?enable leaguerole
+                </UserMsg>
+                <BotMsg>
+                  Listener leaguerole has been enabled
+                </BotMsg>
+              </DiscordBG>
             </li>
-            <li>?disable</li>
-            <p>Takes one argument. It disables a listener and is available only for members with admin perm.
-              Example:</p>
             
-            <p>?disable leaguerole</p>
-            <li>?listeners
+            <li><span className="inlineCode">?disable</span>
+              <p>Takes one argument. It disables a listener and is available only for members with admin perm.
+                Example:</p>
+              <DiscordBG>
+                <UserMsg>
+                  ?disable leaguerole
+                </UserMsg>
+                <BotMsg>
+                  Listener leaguerole has been disabled
+                </BotMsg>
+              </DiscordBG>
+            </li>
+
+            <li><span className="inlineCode">?listeners</span>
               <p>Doesn't take arguments. It lists all listeners and differentiates enabled from disabled
                 listeners</p>
             </li>
           </ul>
         </div>
-    )
+    );
 }
