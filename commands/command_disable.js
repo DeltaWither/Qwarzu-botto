2ce5df320bed1477c820b5fa44bdb7681b72269a
DeltaWither <deltawither@outlook.com>
2022-07-22 00:03:51 +0200

    Removed qwarzu outposto specific checks for ?enable and ?disable

--- commands/command_disable.js
+++ commands/command_disable.js
@@ -5,10 +5,6 @@ const groups = require("../groups/membergroups.js")
 const exec = (message, args) => {
     const listenerName = args[0]
     
-    if (!message.member.roles.cache.hasAny('708716555785076798', '708722040286478356', '945585653024964721', '948632972427423784')) {
-        return
-    }
-    
     if(!individualListeners.hasOwnProperty(listenerName)) {
         message.channel.send(`Listener ${listenerName} doesn't exist`)
         return
