a03dc98f294ce18b0d7291c1111463e29b264dbb
DeltaWither <deltawither@outlook.com>
2022-07-23 15:59:14 +0200

    Made repeat command which uses other commands in its code

---
+++ commands/command_repeat.js
@@ -0,0 +1,33 @@
+const { Command } = require("./Command.js")
+const commandsList = require("./commands.js")
+const groups = require("../groups/membergroups.js")
+
+const exec = async (message, args) => {
+    const returnObj = {
+	string: ""
+    };
+
+    const amount = parseInt(args[0]);
+    if (amount > 20) {
+	return {
+	    string: "Please don't repeat over 20 times"
+	};
+    }
+    const repeatedCommand = commandsList[args[1]];
+    const repeatedCommandArgs = args.slice(2);
+
+
+    for (i = 0; i < amount; i++) {
+	const temp = await repeatedCommand.fullyWrappedExec(message, repeatedCommandArgs);
+	returnObj.string += temp.string;
+	returnObj.string += "\n";
+    }
+
+    return returnObj;
+}
+const description = "";
+
+const repeat = new Command("repeat", description, exec)
+repeat.executeGroup = groups.staff;
+
+module.exports = repeat
