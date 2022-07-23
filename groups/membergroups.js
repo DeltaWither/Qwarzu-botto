9f9b2f2edfc167185fb791ebdbf98ef65c71e92c
DeltaWither <deltawither@outlook.com>
2022-07-23 16:18:16 +0200

    Member groups are now stored in the database

--- groups/membergroups.js
+++ groups/membergroups.js
@@ -1,9 +1,66 @@
-const {MemberGroup} = require("./MemberGroup.js")
-const groups = require("./membergroups.json")
+const { MemberGroup } = require("./MemberGroup.js");
+const database = require("../helper/database.js");
 
 const groupList = {}
 
 module.exports = groupList
+
+let groups = database.read("memberGroups");
+if (!groups) {
+    database.create("memberGroups", [
+	{
+            "name": "everyone",
+            "description": "",
+            "everyone": true
+	},
+	{ 
+            "name": "staff",
+            "description": "",
+            "allowedPerms": [
+		"ADMINISTRATOR"
+            ]
+	},
+	{ 
+            "name": "staffWithoutTrial",
+            "description": "",
+            "allowedPerms": [
+		"ADMINISTRATOR"
+            ]
+	},
+	{ 
+            "name": "admins",
+            "description": "",
+            "everyone": true,
+            "allowedPerms": [
+		"ADMINISTRATOR"
+            ]
+	},
+	{ 
+            "name": "devs",
+            "description": "",
+            "allowedPerms": [
+		"ADMINISTRATOR"
+            ]
+	},
+	{
+            "name": "staffAndDevs",
+            "description": "",
+            "allowedPerms": [
+		"ADMINISTRATOR"
+            ]
+	},
+	{
+            "name": "notNewMembers",
+            "description": "",
+            "allowedPerms": [
+		"ADMINISTRATOR"
+            ]
+	}
+    ]
+		   );
+
+    groups = database.read("memberGroups");
+}
 
 for (group of groups) {
     const newGroup = new MemberGroup(group.name, group.description)
