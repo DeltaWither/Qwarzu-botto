40ab205edb466d7bcc574fd11e8f982bfb38e52c
DeltaWither <deltawither@outlook.com>
2022-07-26 15:28:28 +0200

    ?8ball cleanup

--- commands/command_8ball.js
+++ commands/command_8ball.js
@@ -1,52 +1,47 @@
 const {Command} = require("./Command.js")
 const groups = require("../groups/membergroups.js")
 
+const eightball = [
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes definitely.',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+    'Reply hazy try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    'Don\'t count on it.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.',
+    'No way.',
+    'Maybe',
+    'The answer is hiding inside you',
+    'No.',
+    'Depends on the mood of the CS god',
+    '||No||',
+    '||Yes||',
+    'Hang on',
+    'It\'s over',
+    'It\'s just the beginning',
+    'Good Luck',
+];
+
 const exec = (message, args) => {
     if (!args[0]) {
         return {
 	    string: 'Please ask me a question.'
 	};
     } else {
-        message.channel.sendTyping();
-        let eightball = [
-            'It is certain.',
-            'It is decidedly so.',
-            'Without a doubt.',
-            'Yes definitely.',
-            'You may rely on it.',
-            'As I see it, yes.',
-            'Most likely.',
-            'Outlook good.',
-            'Yes.',
-            'Signs point to yes.',
-            'Reply hazy try again.',
-            'Ask again later.',
-            'Better not tell you now.',
-            'Cannot predict now.',
-            'Concentrate and ask again.',
-            'Don\'t count on it.',
-            'My reply is no.',
-            'My sources say no.',
-            'Outlook not so good.',
-            'Very doubtful.',
-            'No way.',
-            'Maybe',
-            'The answer is hiding inside you',
-            'No.',
-            'Depends on the mood of the CS god',
-            '||No||',
-            '||Yes||',
-            'Hang on',
-            'It\'s over',
-            'It\'s just the beginning',
-            'Good Luck',
-        ];
         let index = (Math.floor(Math.random() * Math.floor(eightball.length)));
-        //setTimeout(() => {
-        //    message.reply({
-        //        content: eightball[index],
-        //    });
-        //}, 750);
 	return {
 	    string: eightball[index]
 	};
