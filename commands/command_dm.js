e095823fe2097f9bb1555123fde04cf67629ea46
DeltaWither <deltawither@outlook.com>
2022-07-26 16:02:47 +0200

    ?dm cleanup using parseMember

--- commands/command_dm.js
+++ commands/command_dm.js
@@ -3,29 +3,20 @@ const id = require("../helper/id.js")
 const groups = require("../groups/membergroups.js")
 
 const exec = async (message, args) => {
-    let userId
-    
     if (!args[0]) {
         return {
 	    string: "No mention"
 	};
     }
     
-    if( await id.isMemberId(args[0], message.guild) ) {
-        userId = args[0]
-    } else if( await id.isMemberMention(args[0], message.guild) ) {
-        userId = args[0].slice(2, -1)
-        if(userId[0] === "!") {
-            userId = userId.slice(1)
-        }
-    } else {
+    const member = await id.parseMember(args[0], message.guild);
+    if (!member) {
         return {
 	    string: "No mention"
 	};
     }
     
     let dmMessage = args.slice(1).join(" ")
-    let member = await message.guild.members.fetch(userId)
     await member.send(dmMessage)
     return {
 	string: `Message sent to ${member.user.username}`
