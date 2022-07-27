999a0f80c642b24f7635dadb4414eba5489c47e5
DeltaWither <deltawither@outlook.com>
2022-07-27 13:00:16 +0200

    ?membergroup cleanup

--- commands/command_membergroup.js
+++ commands/command_membergroup.js
@@ -47,6 +47,9 @@ const permissions = [
     "MODERATE_MEMBERS"
 ];
 
+const possibleFields = ["everyone", "disallowedPerms", "allowedPerms", "disallowedRoles", "allowedRoles",
+			"disallowedMembers", "allowedMembers"];
+
 const exec = async (message, args) => {
     if (args[0] === "describe") {
 	return describe(args.slice(1));
@@ -73,26 +76,23 @@ const describe = (args) => {
 	};
     }
  
-    let returnObj = {
-	string: ""
-    };
     const group = groups[args[0]];
-
     if (!group) {
 	return {
 	    string: `Group ${args[0]} doesn't exist`
 	};
     }
 
+    let returnObj = {
+	string: ""
+    };
+
     //show all necessary fields
     if (group.everyone) {
 	returnObj.string += "everyone: true\n\n"
     }
 
-    const otherFields = ["disallowedPerms", "allowedPerms", "disallowedRoles", "allowedRoles",
-			 "disallowedMembers", "allowedMembers"];
-
-    for (field of otherFields) {
+    for (field of possibleFields.slice(1)) {
 	if (group[field].length !== 0) {
 	    returnObj.string += field + ":\n";
 	    returnObj.string += group[field].toString() + "\n\n";
@@ -109,22 +109,15 @@ const edit = (args) => {
 	};
     }
  
-    let returnObj = {
-	string: ""
-    };
-
     const groupName = args[0];
     const group = groups[groupName];
+    const field = args[1];
     
     if (!group) {
 	return {
 	    string: `Group ${args[0]} doesn't exist`
 	};
     }
-
-    const field = args[1];
-    const possibleFields = ["everyone", "disallowedPerms", "allowedPerms", "disallowedRoles", "allowedRoles",
-			    "disallowedMembers", "allowedMembers"]
 
     if (!possibleFields.includes(field)) {
 	return {
@@ -134,41 +127,12 @@ const edit = (args) => {
 
     //edit everyone
     if (field === "everyone") {
-	group.everyone = (args[2] === "true" ? true : false);
-
-	
-	const groupsData = database.read("memberGroups");
-	groupsData[groupName].everyone = group.everyone;
-	database.update("memberGroups", groupsData);
-	
-	return {
-	    string: `everyone field in group ${group.name} is now ${group.everyone}`
-	};
+	return editEveryone(group, args[2]);
     }
 
     //edit perms
     if (field === "allowedPerms" || field === "disallowedPerms") {
-	const permName = args[2].slice(1).toUpperCase();
-
-	if (args[2][0] === "+") {
-	    if (permissions.includes(permName) && !group[field].includes(permName)) {
-		group[field].push(permName);
-	    }
-	}
-
-	if (args[2][0] === "-") {
-	    group[field] = group[field].filter((value) => {
-		return value !== permName;
-	    });
-	}
-
-	const groupsData = database.read("memberGroups");
-	groupsData[groupName][field] = group[field];
-	database.update("memberGroups", groupsData);
-
-	return {
-	    string: `${field} field in group ${group.name} is now ${group[field].toString()}`
-	};
+	return editPerms(group, field, args[2]);
     }
 
     //edit anything with ids
@@ -176,30 +140,68 @@ const edit = (args) => {
 	field === "disallowedRoles" ||
 	field === "allowedMembers" ||
 	field === "disallowedMembers") {
-	const readId = args[2].slice(1).toUpperCase();
-
-	if (args[2][0] === "+") {
-	    if (id.isId(readId) && !group[field].includes(readId)) {
-		group[field].push(readId);
-	    }
-	}
-
-	if (args[2][0] === "-") {
-	    group[field] = group[field].filter((value) => {
-		return value !== readId;
-	    });
-	}
-
-	const groupsData = database.read("memberGroups");
-	groupsData[groupName][field] = group[field];
-	database.update("memberGroups", groupsData);
-
-	return {
-	    string: `${field} field in group ${group.name} is now ${group[field].toString()}`
-	}
+	return editIdFields(group, field, args[2]);
     }
     
     return {string:"nothing happened"};
+}
+
+const editEveryone = (group, boolString) => {
+    group.everyone = (boolString === "true" ? true : false);
+    editDatabase(group, "everyone");
+    
+    return {
+	string: `everyone field in group ${group.name} is now ${group.everyone}`
+    };
+}
+
+const editPerms = (group, field, permChange) => {
+    const permName = permChange.slice(1).toUpperCase();
+
+    if (permissions.includes(permName)) {
+        addRemoveField(group, field, permChange.toUpperCase());
+	editDatabase(group, field);
+    }
+    
+    return {
+	string: `${field} field in group ${group.name} is now ${group[field].toString()}`
+    };
+}
+
+const editIdFields = (group, field, idChange) => {
+    const readId = idChange.slice(1);
+
+    if (id.isId(readId)) {
+	addRemoveField(group, field, idChange);
+	editDatabase(group, field);
+    }
+
+    return {
+	string: `${field} field in group ${group.name} is now ${group[field].toString()}`
+    }
+}
+
+const addRemoveField = (group, field, change) => {
+    const sign = change[0];
+    const changeElement = change.slice(1);
+
+    if (sign === "+") {
+	if (!group[field].includes(changeElement)) {
+	    group[field].push(changeElement);
+	}
+    }
+
+    if (sign === "-") {
+	group[field] = group[field].filter((value) => {
+	    return value !== changeElement;
+	});
+    }
+}
+
+const editDatabase = (group, field) => {
+    const groupsData = database.read("memberGroups");
+    groupsData[group.name][field] = group[field];
+    database.update("memberGroups", groupsData);
 }
 
 const description = `Usage: ?membergroup describe [group name]
