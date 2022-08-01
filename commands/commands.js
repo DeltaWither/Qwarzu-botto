cad2729da647b51b16713f35af92b78616279957
DeltaWither <deltawither@outlook.com>
2022-08-01 14:24:51 +0200

    Must have forgotten to add this to the commit earlier for loading

--- commands/commands.js
+++ commands/commands.js
@@ -2,17 +2,20 @@ const fs = require("fs")
 
 commandList = {}
 
-//export here to be able to import without circular dependencies
-module.exports = commandList;
+const loadCommands = () => {
+    const files = fs.readdirSync("./commands");
 
-// Not sure why it has to go to ./commands when . is already the commands folder
-const files = fs.readdirSync("./commands");
+    files.forEach(
+	filename => {
+            if(filename.startsWith("command_")) {
+		const command = require("./" + filename)
+		commandList[command.name] = command
+            }
+	}
+    )
+}
 
-files.forEach(
-    filename => {
-        if(filename.startsWith("command_")) {
-            const command = require("./" + filename)
-            commandList[command.name] = command
-        }
-    }
-)
+module.exports = {
+    "commandList": commandList,
+    "loadCommands": loadCommands
+};
