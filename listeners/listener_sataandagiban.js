a58a4fe7381a64ae9e192800161383ab1553575f
DeltaWither <deltawither@outlook.com>
2023-03-17 13:45:35 +0000

    sata andagi ban now only requires andagi and detects cyrillic a

--- listeners/listener_sataandagiban.js
+++ listeners/listener_sataandagiban.js
@@ -2,6 +2,29 @@ const {Listener} = require("./Listener.js");
 const { clientId } = require("../config.json");
 const sendmines = require("../commands/command_sendmines.js");
 
+similarLetters = {
+    "а": "a" // cyrillic a
+}
+
+const changeSimilarLetters = (string) => {
+    let newString = "";
+    for (char of string) {
+        if (similarLetters[char]) {
+            newString += similarLetters[char];
+        }
+        else {
+            newString += char;
+        }
+    }
+    return newString;
+}
+
+const transformMessage = (string) => {
+    let newString = string.toLowerCase();
+    newString = changeSimilarLetters(string);
+    return newString;
+}
+
 const exec = async (message) => {
     if (message.author.id === clientId) {
         return;
@@ -12,12 +35,8 @@ const exec = async (message) => {
         return;
     }
     
-    console.log(message.channelId);
-    
-    if (message.content.toLowerCase().includes("sata andagi") ||
-        message.content.toLowerCase().includes("sataandagi") ||
-        message.content.toLowerCase().includes("satandagi")
-    ) {
+    const transformedMessage = transformMessage(message.content);
+    if (transformedMessage.includes("andagi")) {
         let randomNum = Math.floor(Math.random() * 91) + 10; // between 10 and 100
         let response = await sendmines.wrappedExec(message, [message.author.id, randomNum.toString()], {skipExecGroup: true});
         message.reply(response.string);
