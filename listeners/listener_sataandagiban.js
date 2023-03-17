7f2bed69efb42c826f18cde2d06c34c7de9c1fb8
DeltaWither <deltawither@outlook.com>
2023-03-17 13:57:23 +0000

    more whitespace characters to ignore

--- listeners/listener_sataandagiban.js
+++ listeners/listener_sataandagiban.js
@@ -6,7 +6,7 @@ const similarLetters = {
     "а": "a" // cyrillic a
 }
 
-const whiteSpace = new Set(["\u0020"]);
+const whiteSpace = new Set(["\u000a", "\u0020", "\u00A0", "\u180E", "\u2000", "\u2001", "\u2002", "\u2003", "\u2004", "\u2005", "\u2006", "\u2007", "\u2008", "\u2009", "\u200a", "\u200b", "\u202f", "\u205f", "\u3000", "\ufeff", ""]);
 
 const changeSimilarLetters = (string) => {
     let newString = "";
