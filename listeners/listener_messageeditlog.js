2dd79e2ccccdedf51f1e6a49b509e77fcdbd1cef
DeltaWither <deltawither@outlook.com>
2023-07-07 21:05:11 +0200

    checking old and new messages

--- listeners/listener_messageeditlog.js
+++ listeners/listener_messageeditlog.js
@@ -6,6 +6,7 @@ const exec = async (oldMessage, newMessage) => {
     const editedMessagesChannel = await id.parseChannel("1126945206172909670", newMessage.guild);
     
     console.log(newMessage.editedTimestamp - newMessage.createdTimestamp);
+    console.log(newMessage, oldMessage)
     
     await editedMessagesChannel.send("old: " + oldMessage.content.slice(500) + "\nnew: " + newMessage.content.slice(500) + "\nSent by <@" + newMessage.author.id + ">"
     );
