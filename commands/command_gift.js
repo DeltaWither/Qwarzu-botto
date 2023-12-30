fd5457476a0ee7f67375c5d2ffe640adc6fc51a9
DeltaWither <deltawither@outlook.com>
2023-12-30 19:50:59 +0100

    jolly gift mines command

---
+++ commands/command_gift.js
@@ -0,0 +1,55 @@
+const {Command} = require("./Command.js");
+const groups = require("../groups/membergroups.js");
+const id = require("../helper/id.js");
+const database = require("../helper/database.js");
+const sendmines = require("./command_sendmines.js");
+
+const exec = async (message, args) => {
+	let result = null;
+
+	const requiredChannel = "721071804667527290";
+	const channelId = message.channel.id
+	if (channelId != requiredChannel) {
+		return {
+			string: `You can only gift in <#${requiredChannel}>`
+		};
+	}
+
+	const amount = parseInt(args[1]);
+	if (!amount || amount > 20 || amount < 1) {
+		return {
+			string: "The amount can't be greater than 20"
+		};
+	}
+
+	if (Math.random() < 0.8) {
+		result = await sendmines.wrappedExec(message, [message.author.id, args[1]], {skipExecGroup: true});
+		if (result.string === "Use ?sendmines [user] [amount]" || result.string === "error") {
+			return {
+				string: "Use ?gift [user] [amount]"
+			};
+		}
+		return {
+			string: `The gift backfired, you get ${args[1]} mines`
+		};
+	}
+
+	result = await sendmines.wrappedExec(message, args, {skipExecGroup: true});
+	if (result.string === "Use ?sendmines [user] [amount]" || result.string === "error") {
+		return {
+			string: "Use ?gift [user] [amount]"
+		};
+	}
+
+	return result;
+}
+
+const description = `Usage: ?gift [user] [amount]
+
+Sends a user to the mines with up to 20 mines, but with a high chance to backfire.`;
+
+const gift = new Command("gift", description, exec)
+gift.executeGroup = groups.notNewMembers;
+gift.parent = "mines";
+
+module.exports = gift;
